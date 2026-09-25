00622c5d3582c5bc3e5f16466253b1108a566ce9
Vladislav44-test <elertvelert@gmail.com>
2026-09-25 20:16:31 +0300

    Створив клас Card для представлення ігрової карти, який використовує перечислювані типи для властивостей Value та Suit

---
+++ Card.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CardsAndEnumsTest
+{
+    internal class Card
+    {
+        public Values Value { get; set; }
+        public Suits Suit { get; set; }
+        public string Name { get { return $"{Value} of {Suit}"; } }
+
+        public Card(Values value, Suits suit)
+        {
+            Value = value;
+            Suit = suit;
+        }
+    }
+}
