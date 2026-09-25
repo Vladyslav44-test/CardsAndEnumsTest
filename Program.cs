5009c3a0a45c42b4c582c61377b38635da18ffca
Vladislav44-test <elertvelert@gmail.com>
2026-09-25 20:06:18 +0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CardsAndEnumsTest
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello C# 14.0 and higher!");
+        }
+    }
+}
