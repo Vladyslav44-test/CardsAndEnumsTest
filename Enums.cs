468cf11b685e62993e613a88a5d96e8716c59356
Vladislav44-test <elertvelert@gmail.com>
2026-09-25 20:10:29 +0300

    Додав два перечислення, Suits і Values, для кращої організації типів даних для класу Card

---
+++ Enums.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CardsAndEnumsTest
+{
+    enum Suits
+    {
+        Diamonds,
+        Clubs,
+        Hearts,
+        Spades
+    }
+
+    enum Values : byte
+    {
+        Ace = 1,
+        Two = 2,
+        Three = 3,
+        Four = 4,
+        Five = 5,
+        Six = 6,
+        Seven = 7,
+        Eight = 8,
+        Nine = 9,
+        Ten = 10,
+        Jack = 11,
+        Quenn = 12,
+        King = 13
+    }
+}
