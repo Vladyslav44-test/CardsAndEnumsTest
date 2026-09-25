b69dbda7a817f1b969c0dc3f65bbf3e405c8f816
Vladislav44-test <elertvelert@gmail.com>
2026-09-25 20:26:48 +0300

    Покращив інкапсуляцію властивостей Value та Suit в класі Card для уникнення можливості випадкової зміни значення властивості

--- Card.cs
+++ Card.cs
@@ -6,8 +6,8 @@ namespace CardsAndEnumsTest
 {
     internal class Card
     {
-        public Values Value { get; set; }
-        public Suits Suit { get; set; }
+        public Values Value { get; private set; }
+        public Suits Suit { get; private set; }
         public string Name { get { return $"{Value} of {Suit}"; } }
 
         public Card(Values value, Suits suit)
